b63b6cf2e1b93d569c2912b6f03ace61402f13bd
Jesse Harris <127066580+Brycemight@users.noreply.github.com>
2024-04-14 09:54:46 -0400

    Update Twitch sql.sql

--- Twitch sql.sql
+++ Twitch sql.sql
@@ -1,22 +1,22 @@
 select * 
 from `twich game data`.twitch_game_data tgd 
 
--- What is the top 10 games watched in year 2021 around November? 
+-- What is the top 10 games watched in year 2021 in November? 
 select  
     Game ,
     `Month` ,
     `Year` ,
-    MAX(Hours_watched) AS peak_viewership
+    MAX(Hours_watched) AS Most_watched
 from 
    `twich game data`.twitch_game_data tgd 
 where 
  `Year` =2021 and `Month` =11
 GROUP BY 
     game, month, year
-order by  peak_viewership desc 
+order by  Most_watched desc 
 limit 10;
 
--- What is the top 10 games with the most viewers in year 2021 around November? 
+-- What is the top 10 games with the most viewers in year 2021 in November? 
 
 select  
     Game ,
